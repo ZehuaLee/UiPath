a7c9817dc5fb95dac33415c33011b8cf81509c55
Mihai Pantea <mihai.pantea@uipath.com>
2019-10-03 11:56:12 +0300

    Refactor code

--- QRWriter/UiPath.QRCodeLib.Activities.cs
+++ QRWriter/UiPath.QRCodeLib.Activities.cs
@@ -1,281 +1,138 @@
-﻿using System;
-using System.Activities;
+﻿using System.Activities;
 using System.ComponentModel;
-using ZXing;
-using ZXing.QrCode;
-using ZXing.Common;
+using System.Diagnostics;
 using System.Drawing;
-using System.IO;
 using UiPath.Core;
+using Image = System.Drawing.Image;
 
 namespace QRCodeLib
 {
-
     public class QRReadFromPath : CodeActivity
     {
-        [Category("Input")]
         [RequiredArgument]
-        [Description("The path of the qr code picture")]
-        public InArgument<string> QRpath { get; set; }
+        [Category("Input")]
+        [Description("A path to an image file containing a QR code")]
+        public InArgument<string> QRImage { get; set; }
 
         [Category("Output")]
-        [Description("Message string hidden inside the qr code")]
+        [Description("The information decoded from the provided QR code")]
         public OutArgument<string> Message { get; set; }
-
-        /* This function is to read the information from a picture file
-         * argument: string 
-         * return: string
-         */
-        protected string QrReader(string imagePath)
-        {
-            string result = "";
-            
-            // extract the picture file into image.
-            MultiFormatReader multiReader = new MultiFormatReader();
-            Bitmap img = (Bitmap)Bitmap.FromFile(imagePath);
-            if (img == null)
-            {
-                throw (new Exception("No image exists"));
-            }
-
-            // transfer the image into binarybitmap
-            LuminanceSource ls = new BitmapLuminanceSource(img);
-            BinaryBitmap bbmp = new BinaryBitmap(new HybridBinarizer(ls));
-
-            // get the information hidden in Qr code.
-            Result res = multiReader.decode(bbmp);
-            result = res.ToString();
-            return result;
-        }
 
         protected override void Execute(CodeActivityContext context)
         {
-            var ImagePath = QRpath.Get(context);
-            var result = QrReader(ImagePath);
-            Message.Set(context, result);
-            Console.WriteLine($"QR code read from {ImagePath} finished.");
+            string path = QRImage.Get(context);
+            string message = MessageExtractor.ExtractFromPath(path);
+
+            Message.Set(context, message);
+            Trace.TraceInformation($"Finished reading QR code from path: '{path}'.");
         }
     }
-
-
 
     public class QRReadFromImage : CodeActivity
     {
-        [Category("Input")]
         [RequiredArgument]
-        [Description("The image object of a qr code picture")]
-        public InArgument<System.Drawing.Image> QRImage { get; set; }
+        [Category("Input")]
+        [Description("An image object containing a QR code")]
+        public InArgument<Image> QRImage { get; set; }
 
         [Category("Output")]
-        [Description("Message string hidden inside the qr code")]
+        [Description("The information decoded from the provided QR code")]
         public OutArgument<string> Message { get; set; }
-
-        /* This function is to read the information from a qr code image
-         * argument: image 
-         * return: string
-         */
-        protected string QrReader(System.Drawing.Image imageqr)
-        {
-            string result = "";
-
-            MultiFormatReader multiReader = new MultiFormatReader();
-            Bitmap img = (Bitmap)imageqr;
-            if (img == null)
-            {
-                throw (new Exception("No image exists"));
-            }
-            LuminanceSource ls = new BitmapLuminanceSource(img);
-            BinaryBitmap bbmp = new BinaryBitmap(new HybridBinarizer(ls));
-            Result res = multiReader.decode(bbmp);
-            result = res.ToString();
-            return result;
-        }
 
         protected override void Execute(CodeActivityContext context)
         {
-            var Imageqr = QRImage.Get(context);
-            var result = QrReader(Imageqr);
-            Message.Set(context, result);
+            Image image = QRImage.Get(context);
+            string message = MessageExtractor.ExtractFromBitmap(image as Bitmap);
+
+            Message.Set(context, message);
+            Trace.TraceInformation("Finished reading QR code from image.");
         }
     }
-
     
     public class QRReadFromUiElement : CodeActivity
     {
+        [RequiredArgument]
         [Category("Input")]
-        [Description("The target UiElement containing qr code")]
+        [Description("The target UiElement containing a QR code")]
         public InArgument<UiElement> UiElement { get; set; }
         
         [Category("Output")]
         [Description("Message string hidden inside the qr code")]
         public OutArgument<string> Message { get; set; }
 
-        /* This function is to read the information from a qr code image
-         * argument: image 
-         * return: string
-         */
-        protected string QrReader(System.Drawing.Image imageqr)
-        {
-            string result = "";
-
-            MultiFormatReader multiReader = new MultiFormatReader();
-            Bitmap img = (Bitmap)imageqr;
-            if (img == null)
-            {
-                throw (new Exception("No image exists"));
-            }
-            LuminanceSource ls = new BitmapLuminanceSource(img);
-            BinaryBitmap bbmp = new BinaryBitmap(new HybridBinarizer(ls));
-            Result res = multiReader.decode(bbmp);
-            result = res.ToString();
-            return result;
-        }
-        
-        /*This function is to catch the wanted area according to UiElement
-         * argument: UiElement
-         * return: image 
-         */
-        private System.Drawing.Image screenshoot(UiElement ui) {
-
-            // Catch the whole screen and save in memory
-            Size screen = new Size(System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width, System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height);
-            Bitmap memoryImage = new Bitmap(screen.Width, screen.Height);
-            Graphics memoryGraphics = Graphics.FromImage(memoryImage);
-            
-            // position the rectangle area where QRcode exists 
-            Point startPoint = new Point(0,0);
-            Point destPoint = new Point(0,0);
-            destPoint = ui.GetPosition().Rectangle.Value.Location;
-            destPoint.X += ui.GetPosition().Rectangle.Value.Width;
-            destPoint.Y += ui.GetPosition().Rectangle.Value.Height;
-            startPoint = ui.GetPosition().Rectangle.Value.Location;
-
-            // get only the specific rectangle that contains Qrcode
-            memoryGraphics.CopyFromScreen( destPoint, startPoint, ui.GetPosition().Rectangle.Value.Size);
-            return (System.Drawing.Image)memoryImage;
-        }
-
         protected override void Execute(CodeActivityContext context)
         {
-            UiElement uielem = UiElement.Get(context);
-            string base64 = uielem.ImageBase64;
-            byte[] bytes = Convert.FromBase64String(base64);
-            System.Drawing.Image img = System.Drawing.Image.FromStream(new MemoryStream(bytes));
-            var result = QrReader(img);
+            UiElement uiElement = UiElement.Get(context);
+            string result = MessageExtractor.ExtractFromUiElement(uiElement);
+
             Message.Set(context, result);
+            Trace.TraceInformation($"Finished reading QR code from UiElement: {uiElement.Selector}.");
         }
     }
 
     public class QRWriteToImage : CodeActivity
     {
-        [Category("Input")]
         [RequiredArgument]
-        [Description("Message string to be hidden in qr code")]
+        [Category("Input")]
+        [Description("The message to be encoded in the QR code")]
         public InArgument<string> Message { get; set; }
 
         [Category("Input")]
-        [Description("The width of output image")]
+        [Description("The width of the output image")]
+        [DefaultValue(250)]
         public InArgument<int> Width { get; set; }
 
         [Category("Input")]
-        [Description("The height of output image")]
+        [Description("The height of the output image")]
+        [DefaultValue(250)]
         public InArgument<int> Height { get; set; }
 
         [Category("Output")]
-        [Description("The output image of qr code")]
-        public OutArgument<System.Drawing.Image> Result { get; set; }
-
-        /*This function is to hide information to a size-specified qr code image.
-         * argument: string, int, int
-         * return: image 
-         */
-        protected System.Drawing.Image QrWriter(string info, int width = 0, int height = 0)
-        {
-            if (width <= 0 || height <= 0)
-            {
-                width = 250;
-                height = 250;
-            }
-            BarcodeWriter writer = new BarcodeWriter();
-            var options = new QrCodeEncodingOptions();
-            options = new QrCodeEncodingOptions
-            {
-                DisableECI = true,
-                CharacterSet = "UTF-8",
-                Width = width,
-                Height = height,
-            };
-            writer.Format = BarcodeFormat.QR_CODE;
-            writer.Options = options;
-            var bitmap = new Bitmap(writer.Write(info));
-            return bitmap;
-        }
+        [Description("The output image")]
+        public OutArgument<Image> Result { get; set; }
 
         protected override void Execute(CodeActivityContext context)
         {
-            var message = Message.Get(context);
-            var width = Width.Get(context);
-            var height = Height.Get(context);
-            var Succeed = QrWriter(message, width, height);
-            Result.Set(context, Succeed);
+            string message = Message.Get(context);
+            int width = Width.Get(context);
+            int height = Height.Get(context);
+
+            Image image = QrCodeDrawer.Draw(message, width, height);
+            Result.Set(context, image);
         }
     }
 
     public class QRWriteToPath : CodeActivity
     {
-        [Category("Input")]
         [RequiredArgument]
-        [Description("The path to store the output qr code picture")]
+        [Category("Input")]
+        [Description("The message to be encoded in the QR code")]
+        public InArgument<string> Message { get; set; }
+
+        [RequiredArgument]
+        [Category("Input")]
+        [Description("The path to save the output image")]
         public InArgument<string> Path { get; set; }
 
         [Category("Input")]
-        [RequiredArgument]
-        [Description("The message inside the qr code")]
-        public InArgument<string> Message { get; set; }
-
-        [Category("Input")]
-        [Description("The width of output image")]
+        [Description("The width of the output image")]
         public InArgument<int> Width { get; set; }
 
         [Category("Input")]
-        [Description("The height of output image")]
+        [Description("The height of the output image")]
         public InArgument<int> Height { get; set; }
-
-        /*This function is to hide information to a size-specified qr code image.
-         * argument: string, int, int
-         * return: image 
-         */
-        protected System.Drawing.Image QrWriter(string info, int width = 0, int height = 0)
-        {
-            if (width <= 0 || height <= 0)
-            {
-                width = 250;
-                height = 250;
-            }
-            BarcodeWriter writer = new BarcodeWriter();
-            var options = new QrCodeEncodingOptions();
-            options = new QrCodeEncodingOptions
-            {
-                DisableECI = true,
-                CharacterSet = "UTF-8",
-                Width = width,
-                Height = height,
-            };
-            writer.Format = BarcodeFormat.QR_CODE;
-            writer.Options = options;
-            var bitmap = new Bitmap(writer.Write(info));
-            return bitmap;
-        }
 
         protected override void Execute(CodeActivityContext context)
         {
-            var Imagepath = Path.Get(context);
-            var message = Message.Get(context);
-            var width = Width.Get(context);
-            var height = Height.Get(context);
-            var Succeed = QrWriter(message, width, height);
-            Succeed.Save(Imagepath);
+            string path = Path.Get(context);
+            string message = Message.Get(context);
+            int width = Width.Get(context);
+            int height = Height.Get(context);
+
+            using (Image image = QrCodeDrawer.Draw(message, width, height))
+            {
+                image.Save(path);
+            }
         }
     }
-
 }
